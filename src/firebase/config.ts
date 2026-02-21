0275d778d7506e12d0c7f24c7e4ff526680f9322
Hemang Joshi <www.hemangjoshi@gmail.com>
2026-02-21 05:47:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9454171627-e6e05",
+  "appId": "1:490594584235:web:0e746c6fd9f55e940559ee",
+  "apiKey": "AIzaSyDiIkPKDUS_MGD4zoQCV_h_2DwnBEDKaOo",
+  "authDomain": "studio-9454171627-e6e05.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "490594584235"
+};
